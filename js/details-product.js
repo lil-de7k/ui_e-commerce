3f3ea6fc646c6fe8dd54dd6a2cc74bbc47549594
Islam rabea <rabeaeslam56@gmail.com>
2025-02-03 13:31:57 +0200

    add code in details-product.js file

--- js/details-product.js
+++ js/details-product.js
@@ -1,3 +1,44 @@
+// - show details product using query params
+async function generateDetails(id) {
+  let result = await fetch(`http://localhost:3000/products?userId=${id}`);
+  let data = await result.json();
+
+  handleData(data);
+}
+
+let details = document.querySelector(".details-list");
+function handleData(data) {
+  details.innerHTML = data
+    .map((item) => {
+      let { title, img, body, price, quantity } = item;
+
+      return `
+      <li>
+        <figure>
+          <img src="${img}" alt="" />
+        </figure>
+        <div class="details-title">
+          <h3>${title}</h3>
+          <p>${body}</p>
+          <span>Price: ${price}</span>
+          <div class="quantity">
+            <p>quantity: </p>
+            <input type="text" value="${quantity}" />
+          </div>
+          <button>Add To Cart</button>
+        </div>
+      </li>
+    
+    `;
+    })
+    .join("");
+}
+
+export { generateDetails };
+
+/*
+- show details product using find
+
 async function generateDetails(id) {
   let result = await fetch("data-details.json");
   let data = await result.json();
@@ -29,3 +70,4 @@ function handleData(data) {
 }
 
 export { generateDetails };
+*/
