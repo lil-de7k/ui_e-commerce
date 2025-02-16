f74c463dc9c526c431d2b8bd2f26833e525f3f81
Islam rabea <rabeaeslam56@gmail.com>
2025-02-17 00:36:02 +0200

    update in show-details file

--- js/details-product.js
+++ js/details-product.js
@@ -1,3 +1,4 @@
+/*
 // - show details product using query params
 async function generateDetails(id) {
   let result = await fetch(`http://localhost:3000/products?userId=${id}`);
@@ -32,34 +33,26 @@ function handleData(data) {
     `;
     })
     .join("");
-
-  let input = document.querySelector("input");
-  let id = Number(input.getAttribute("data-id"));
-
-  let getData = JSON.parse(localStorage.getItem("products")) || [];
-  let product = getData.find((ele) => ele.id === id);
-
-  if (product) {
-    input.value = product.quantity;
-  }
 }
 
 export { generateDetails };
+*/
 
-/*
-- show details product using find
+// - show details product using find
 
 async function generateDetails(id) {
   let result = await fetch("data-details.json");
   let data = await result.json();
 
-  let product = data.find((ele) => ele.id === id);
-  handleData(product);
+  let product = data.products.find((ele) => ele.id === id);
+  if (product) {
+    handleData(product);
+  }
 }
 
 let details = document.querySelector(".details-list");
 function handleData(data) {
-  let { title, img, body, price, quantity } = data;
+  let { id, title, img, body, price, quantity } = data;
   details.innerHTML = `
       <li>
         <figure>
@@ -71,13 +64,22 @@ function handleData(data) {
           <span>Price: ${price}</span>
           <div class="quantity">
             <p>quantity: </p>
-            <input type="text" value="${quantity}" />
+            <input type="text" data-id="${id}" value="${quantity}" />
           </div>
           <button>Add To Cart</button>
         </div>
       </li>
     `;
+
+  let input = document.querySelector("input");
+  let productId = Number(input.getAttribute("data-id"));
+
+  let getData = JSON.parse(localStorage.getItem("products")) || [];
+  let product = getData.find((ele) => ele.id === productId);
+
+  if (product) {
+    input.value = product.quantity;
+  }
 }
 
 export { generateDetails };
-*/
